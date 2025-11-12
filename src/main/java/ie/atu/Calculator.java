1770fbb13199d9d94caf5456aa1d47533f4c58a1
EthanLourens-STUDENT <G00467054@atu.ie>
2025-11-12 17:13:05 +0000

    Multiply Fail

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -16,7 +16,10 @@ public class Calculator {
     }
 
     public int mul(int i, int i1) {
-        return i * i1;
+        if(i >= Integer.MAX_VALUE || i1 >= Integer.MAX_VALUE)
+            throw new ArithmeticException("Number cannot be greater than or equal to Integer.MAX_VALUE");
+        int result = i*i1;
+        return result;
     }
 
     public int div(int i, int i1) {
